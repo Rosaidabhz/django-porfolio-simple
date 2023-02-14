2ec466402724aed50818d5c437f82f51dce7b9da
rosaidabhz <rosaidabhz@gmail.com>
2023-02-14 12:40:16 -0500

    projects

--- portfolio/static/materialize.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elemsparallax = document.querySelectorAll('.parallax');
-    var instancesparallax = M.Parallax.init(elemsparallax);
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {indicators:true});
-    var elemssidenav = document.querySelectorAll('.sidenav');
-    var instancessidenav = M.Sidenav.init(elemssidenav);
-  });
